bd5f11a0cea3e5e506de7fa35505d8e9673260ed
ChuHieu <chuhieu020297@gmail.com>
2018-10-30 14:37:55 +0700

    Initial commit

---
+++ app/src/main/java/com/poly/chuhieu/pt13355_duan1_hieucvph06411/MainActivity.java
@@ -0,0 +1,13 @@
+package com.poly.chuhieu.pt13355_duan1_hieucvph06411;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
