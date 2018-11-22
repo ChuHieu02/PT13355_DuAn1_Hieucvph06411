d2422a659b72af433bacac524cef7be42b17326b
ChuHieu <chuhieu020297@gmail.com>
2018-11-22 09:35:28 +0700

    update code

--- app/src/main/java/com/poly/chuhieu/pt13355_duan1_hieucvph06411/DangNhap_Activity.java
+++ app/src/main/java/com/poly/chuhieu/pt13355_duan1_hieucvph06411/DangNhap_Activity.java
@@ -1,13 +1,23 @@
 package com.poly.chuhieu.pt13355_duan1_hieucvph06411;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.AutoCompleteTextView;
+
+import android.widget.EditText;
+
+
+
+public class DangNhap_Activity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dangnhap);
+
     }
 }
+
